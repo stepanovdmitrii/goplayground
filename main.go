66a51ca5ae138d59d2cffc182698d92a22d26410
Dmitrii <dima-stepan@yandex.ru>
2022-03-06 15:24:30 +0300

    emb

--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import "fmt"
 
-func appendInt64(sl []int64) {
-	sl = append(sl, 1000)
-	fmt.Printf("%v\n", sl)
+type Inner struct {
+
 }
 
+func (s *Inner) Hello() {
+	fmt.Println("inner")
+}
+
+
+type Outer struct {
+	Inner
+}
+
+func (s *Outer) Hello() {
+	fmt.Println("outer")
+}
+
+
+
 func main() {
-	sl := make([]int64, 0, 10)
-	for i := 0; i < 9; i++ {
-		sl = append(sl, int64(i))
-	}
-	appendInt64(sl)
-	fmt.Printf("%v\n", sl)
+	o := &Outer{}
+	o.Hello()
+	o.Inner.Hello()
 }
