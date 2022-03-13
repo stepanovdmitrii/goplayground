35562abe18d61b871fadd9010456d2d297ce0252
Dmitrii <dima-stepan@yandex.ru>
2022-03-13 16:25:14 +0300

    channels

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
-type Interface1 interface {
-	Method() string
-}
-
-type Interface2 interface {
-	Method() string
-}
-
-type Interface3 interface {
-	Interface1
-	Interface2
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	var i Interface3
-	i.Method()
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		time.Sleep(time.Second * 5)
+		ch2 <- 100
+		time.Sleep(time.Second * 5)
+		close(ch2)
+	}()
+	close(ch1)
+	for {
+		select {
+		case _, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+				break
+			}
+			fmt.Println("from ch1")
+		case _, ok := <-ch2:
+			if !ok {
+				ch2  = nil
+				break
+			}
+			fmt.Println("from ch2")
+		}
+		if ch1 == nil && ch2 == nil {
+			break
+		}
+	}
 }
