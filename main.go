91929c48de23139ffc505b8f9f4516db4fd139ed
Dmitrii <dima-stepan@yandex.ru>
2022-08-11 23:02:21 +0300

    context cancel

--- main.go
+++ main.go
@@ -1,48 +1,29 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
+	"time"
 )
 
-type userError struct{}
-
-func (e *userError) Error() string {
-	return "Пользовательская ошибка"
-}
-
-type errorWithCause struct {
-	msg   string
-	cause error
-}
-
-func (e *errorWithCause) Error() string {
-	return e.msg
-}
-
-func (e *errorWithCause) Cause() error {
-	return e.cause
-}
-
-func (e *errorWithCause) Unwrap() error {
-	return e.cause
-}
-
-func NewErrorWithCause(msg string, err error) error {
-	return &errorWithCause{cause: err, msg: msg}
-}
-
-var userErr = &userError{}
-
-func getWrappedError() error {
-	return NewErrorWithCause("Обёртка", userErr)
-}
-
 func main() {
-	err := getWrappedError()
-	if errors.Is(err, userErr) {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println("Это не пользовательская ошибка")
-	}
+	fmt.Printf("Запуск: %v\n", time.Now())
+	ctx1, cancel := context.WithCancel(context.Background())
+
+	go func(ctx context.Context) {
+		ctx2, cancel2 := context.WithTimeout(ctx, 3*time.Second) //попробуй поставить тут 3 или 10 секунд
+		defer cancel2()
+
+		select {
+		case <-ctx2.Done():
+			fmt.Printf("Контекст отменен: %v\n", time.Now())
+		}
+
+	}(ctx1)
+
+	time.Sleep(5 * time.Second)
+	fmt.Printf("Отмена: %v\n", time.Now())
+	cancel()
+
+	time.Sleep(7)
 }
